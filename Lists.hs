23c6431247fc92186fab7404d59dd80e99bae4f1
lorenzHeinrich <lorenz.heinrich@student.kit.edu>
2024-09-21 15:22:32 +0200

    complete list problems

--- Lists.hs
+++ Lists.hs
@@ -170,7 +170,7 @@ encode xs = map (\x -> (length x, head x)) (pack xs)
 
 -- Problem 11
 -- Modified run-length encoding.
- 
+
 -- Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list.
 -- Only elements with duplicates are transferred as (N E) lists.
 -- Example:
@@ -224,11 +224,11 @@ encodeDirect (x:xs) = packOrPrepend x (encodeDirect xs)
     where
         packOrPrepend x [] = [Single x]
         packOrPrepend x (s@(Single y):ys)
-            | x == y    = Multiple 2 y:ys 
+            | x == y    = Multiple 2 y:ys
             | otherwise = Single x : s : ys
         packOrPrepend x (m@(Multiple n y):ys)
             | x == y    = Multiple (n + 1) y : ys
-            | otherwise =  Single x : m : ys 
+            | otherwise =  Single x : m : ys
 
 -- Problem 14
 -- Duplicate the elements of a list.
@@ -316,13 +316,13 @@ slice xs i k = take (k-i) (drop (i-1) xs)
 -- λ> rotate ['a','b','c','d','e','f','g','h'] (-2)
 -- "ghabcdef"
 rotate :: [a] -> Int -> [a]
-rotate xs n = 
+rotate xs n =
     let i = (length xs + n) `mod` length xs
     in drop i xs ++ take i xs
 
 -- Problem 20
 -- Remove the K'th element from a list.
- 
+
 -- Example in Prolog:
 -- ?- remove_at(X,[a,b,c,d],2,R).
 -- X = b
@@ -341,7 +341,7 @@ removeAt i xs = (xs !! (i-1), take (i-1) xs ++ drop i xs)
 
 -- Problem 21
 -- Insert an element at a given position into a list.
- 
+
 -- Example:
 -- * (insert-at 'alfa '(a b c d) 2)
 -- (A ALFA B C D)
@@ -369,7 +369,7 @@ range a b = [a..b]
 
 -- Problem 26
 -- Generate combinations of K distinct objects chosen from the N elements of a list.
- 
+
 -- In how many ways can a committee of 3 be chosen from a group of 12 people?
 -- We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficients).
 -- For pure mathematicians, this result may be great.
@@ -383,4 +383,83 @@ range a b = [a..b]
 
 -- λ> combinations 3 "abcdef"
 -- ["abc","abd","abe",...]
--- combinations :: Int 
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 xs = [[]]
+combinations k l@(x:xs)
+    | k == length l = [l]
+    | otherwise     = map (x :) (combinations (k-1) xs) ++ combinations k xs
+
+-- Problem 27
+-- Group the elements of a set into disjoint subsets.
+--
+-- a) In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons?
+-- Write a function that generates all the possibilities and returns them in a list.
+--
+-- Example:
+-- * (group3 '(aldo beat carla david evi flip gary hugo ida))
+-- ( ( (ALDO BEAT) (CARLA DAVID EVI) (FLIP GARY HUGO IDA) )
+-- ... )
+group3 :: Eq a => [a] -> [[[a]]]
+group3 xs = addThrees (combinations 4 xs) xs
+    where
+        -- Überprüft, ob ein Element in der Liste ist
+        isIn :: Eq a => [a] -> a -> Bool
+        isIn [] _     = False
+        isIn (x:xs) y = x == y || isIn xs y
+
+        -- filter ys for elements not in xs --
+        filtered :: Eq a => [a] -> [a] -> [a]
+        filtered xs = filter (not . isIn xs)
+
+        addThrees :: Eq a => [[a]] -> [a] -> [[[a]]]
+        addThrees fours xs = concatMap (\four -> addThreesToFour four (combinations 3 (filtered four xs)) xs) fours
+
+        addThreesToFour :: Eq a => [a] -> [[a]] -> [a] -> [[[a]]]
+        addThreesToFour _ [] xs                = []
+        addThreesToFour four (three:threes) xs = [rest four three xs, three, four] : addThreesToFour four threes xs
+            where 
+                rest :: Eq a => [a] -> [a] -> [a] -> [a]
+                rest four three xs = filtered four (filtered three xs)
+
+-- b) Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return a list of groups.
+
+-- Example:
+-- * (group '(aldo beat carla david evi flip gary hugo ida) '(2 2 5))
+-- ( ( (ALDO BEAT) (CARLA DAVID) (EVI FLIP GARY HUGO IDA) )
+-- ... )
+-- Note that we do not want permutations of the group members; i.e. ((ALDO BEAT) ...) is the same solution as ((BEAT ALDO) ...). However, we make a difference between ((ALDO BEAT) (CARLA DAVID) ...) and ((CARLA DAVID) (ALDO BEAT) ...).
+
+-- You may find more about this combinatorial problem in a good book on discrete mathematics under the term "multinomial coefficients".
+-- Example in Haskell:
+
+-- λ> group [2,3,4] ["aldo","beat","carla","david","evi","flip","gary","hugo","ida"]
+-- [[["aldo","beat"],["carla","david","evi"],["flip","gary","hugo","ida"]],...]
+-- (altogether 1260 solutions)
+
+-- λ> group [2,2,5] ["aldo","beat","carla","david","evi","flip","gary","hugo","ida"]
+-- [[["aldo","beat"],["carla","david"],["evi","flip","gary","hugo","ida"]],...]
+-- (altogether 756 solutions)
+
+group :: Eq a => [Int] -> [a] -> [[[a]]] 
+group [first, second, third] xs
+    | length xs == first + second + third = 
+        concatMap (\f -> addToFirst f (combinations second (filtered f xs)) xs) (combinations first xs)
+    | otherwise = error "sum of group sizes must match list length"
+    where
+        addToFirst :: Eq a => [a] -> [[a]] -> [a] -> [[[a]]]
+        addToFirst _ [] _           = []
+        addToFirst first (second:seconds) xs = 
+            [first, second, rest first second xs] : addToFirst first seconds xs
+            where 
+                rest first second xs = filtered first (filtered second xs)
+
+        -- Überprüft, ob ein Element in der Liste ist
+        isIn :: Eq a => [a] -> a -> Bool
+        isIn [] _     = False
+        isIn (x:xs) y = x == y || isIn xs y
+
+        -- filter ys for elements not in xs --
+        filtered :: Eq a => [a] -> [a] -> [a]
+        filtered xs = filter (not . isIn xs)
+
